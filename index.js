4565a15e819d4850cf2a08d0d18b15e101f2530f
amarsanaa.e <amarsanaa.e@itc.gov.mn>
2023-12-05 04:11:14 +0800

    chat

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const io = new Server(8000, {
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
+let firstMemberSocketId = null;
+let firstMemberSocketMail = null;
+let secondMemberSocketId = null;
+let secondMemberSocketMail = null;
+
 
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
   socket.on("room:join", (data) => {
     const { email, room } = data;
+    if(!firstMemberSocketId){
+      firstMemberSocketId = socket.id;
+      firstMemberSocketMail = email;
+    }else{
+      secondMemberSocketId = socket.id;
+      secondMemberSocketMail = email;
+    }
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
     io.to(room).emit("user:joined", { email, id: socket.id });
@@ -34,5 +46,34 @@ io.on("connection", (socket) => {
   socket.on("peer:nego:done", ({ to, ans }) => {
     console.log("peer:nego:done", ans);
     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+
+  socket.on("message:sendMsg", (to) => {
+    const {message, remoteSocketId } = to;
+    console.log(to);
+    console.log(secondMemberSocketId, firstMemberSocketId);
+    emailToSocketIdMap.set(message, secondMemberSocketId);
+    socketidToEmailMap.set(secondMemberSocketId, message);
+    if(secondMemberSocketId === to.remoteSocketId){
+      io.to(secondMemberSocketId).emit("user:sendMsg", { message, id: firstMemberSocketMail });
+
+      emailToSocketIdMap.set(message, firstMemberSocketId);
+      socketidToEmailMap.set(firstMemberSocketId, message);
+      io.to(firstMemberSocketId).emit("user:sendMsg1", { message, id: firstMemberSocketMail });
+    }else if(firstMemberSocketId === to.remoteSocketId){
+      io.to(secondMemberSocketId).emit("user:sendMsg", { message, id: firstMemberSocketMail });
+
+      emailToSocketIdMap.set(message, firstMemberSocketId);
+      socketidToEmailMap.set(firstMemberSocketId, message);
+      io.to(firstMemberSocketId).emit("user:sendMsg1", { message, id: firstMemberSocketMail });
+    }else{
+      io.to(secondMemberSocketId).emit("user:sendMsg", { message, id: secondMemberSocketMail });
+
+      emailToSocketIdMap.set(message, firstMemberSocketId);
+      socketidToEmailMap.set(firstMemberSocketId, message);
+      io.to(firstMemberSocketId).emit("user:sendMsg1", { message, id: secondMemberSocketMail });
+    }
+    
+    
   });
 });
